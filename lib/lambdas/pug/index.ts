8d9c0c1061868d3d27a21339a5a82a53126d5cef
David Yoon <DYoonity@users.noreply.github.com>
2024-07-12 12:59:29 -0400

    cats body change

--- lib/lambdas/pug/index.ts
+++ lib/lambdas/pug/index.ts
@@ -1,11 +1,13 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-export const handler = async function (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
-  console.log("request:", JSON.stringify(event, undefined, 4));
+export const handler = async function (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> {
+  console.log('request:', JSON.stringify(event, undefined, 4));
   const stageName = process.env.STAGE_NAME!;
   return {
     statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `The pugs and the beagles like the ${stageName}`,
+    headers: { 'Content-Type': 'text/plain' },
+    body: `The cats and the beagles like the ${stageName}`,
   };
 };
