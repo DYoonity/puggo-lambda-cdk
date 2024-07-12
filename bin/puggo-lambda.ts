cdf08ba2b9147b76b631708ef7380097aaeca1f7
David Yoon <DYoonity@users.noreply.github.com>
2024-07-12 12:35:56 -0400

    Initial commit

--- bin/puggo-lambda.ts
+++ bin/puggo-lambda.ts
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-import { PuggoCodePipelineStack } from "../lib/codepipeline-stack";
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { PuggoCodePipelineStack } from '../lib/codepipeline-stack';
 
 const app = new cdk.App();
 
-new PuggoCodePipelineStack(app, "PuggoCodePipelineStack", {
+new PuggoCodePipelineStack(app, 'PuggoCodePipelineStack', {
   env: {
-    account: "XXXXX",
-    region: "us-east-1",
+    account: '693645687349',
+    region: 'us-east-1',
   },
-  stackName: "PuggoCodePipelineStack",
-  description: "Testing App for Lambda Deployments",
+  stackName: 'PuggoCodePipelineStack',
+  description: 'Testing App for Lambda Deployments',
 });
--- lib/codepipeline-stack.ts
+++ lib/codepipeline-stack.ts
@@ -1,50 +1,63 @@
-import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
-import * as pipelines from "aws-cdk-lib/pipelines";
-import { PuggoLambdaStack } from "./puggo-lambda-stack";
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as pipelines from 'aws-cdk-lib/pipelines';
+import { PuggoLambdaStack } from './puggo-lambda-stack';
 
 export class PuggoCodePipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const githubAccessToken = cdk.SecretValue.secretsManager("github-access-token-name");
+    const githubAccessToken =
+      cdk.SecretValue.secretsManager('cdk-github-token');
 
-    const pipeline = new pipelines.CodePipeline(this, "PuggoCodePipeline", {
-      pipelineName: "PuggoCodePipeline",
-      synth: new pipelines.ShellStep("Synth", {
-        input: pipelines.CodePipelineSource.gitHub("github-repository-name", "main", {
-          authentication: githubAccessToken,
-        }),
-        commands: ["npm i", "npm run build-all", "npx cdk synth"],
+    const pipeline = new pipelines.CodePipeline(this, 'PuggoCodePipeline', {
+      pipelineName: 'PuggoCodePipeline',
+      synth: new pipelines.ShellStep('Synth', {
+        input: pipelines.CodePipelineSource.gitHub(
+          'DYoonity/puggo-lambda-cdk',
+          'main',
+          {
+            authentication: githubAccessToken,
+          },
+        ),
+        commands: ['npm i', 'npm run build-all', 'npx cdk synth'],
       }),
     });
 
-    const stagingStage = new cdk.Stage(this, "StagingStage");
-    const lambdaStackStaging = new PuggoLambdaStack(stagingStage, "PuggoLambdaStack-Staging", {
-      env: {
-        account: "XXXXX",
-        region: "us-east-1",
+    const stagingStage = new cdk.Stage(this, 'StagingStage');
+    const lambdaStackStaging = new PuggoLambdaStack(
+      stagingStage,
+      'PuggoLambdaStack-Staging',
+      {
+        env: {
+          account: '693645687349',
+          region: 'us-east-1',
+        },
+        stackName: 'PuggoLambdaStack-Staging',
+        description: 'Testing App for Lambda Staging',
+        stageName: 'staging',
       },
-      stackName: "PuggoLambdaStack-Staging",
-      description: "Testing App for Lambda Staging",
-      stageName: "staging",
-    });
+    );
 
     pipeline.addStage(stagingStage);
 
-    const prodStage = new cdk.Stage(this, "ProductionStage");
-    const lambdaStackProd = new PuggoLambdaStack(prodStage, "PuggoLambdaStack-Prod", {
-      env: {
-        account: "XXXXX",
-        region: "us-east-1",
+    const prodStage = new cdk.Stage(this, 'ProductionStage');
+    const lambdaStackProd = new PuggoLambdaStack(
+      prodStage,
+      'PuggoLambdaStack-Prod',
+      {
+        env: {
+          account: '693645687349',
+          region: 'us-east-1',
+        },
+        stackName: 'PuggoLambdaStack-Prod',
+        description: 'Testing App for Lambda Prod',
+        stageName: 'production',
       },
-      stackName: "PuggoLambdaStack-Prod",
-      description: "Testing App for Lambda Prod",
-      stageName: "production",
-    });
+    );
 
     pipeline.addStage(prodStage, {
-      pre: [new pipelines.ManualApprovalStep("PromoteToProd")],
+      pre: [new pipelines.ManualApprovalStep('PromoteToProd')],
     });
   }
 }
